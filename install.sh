4ad68cd78f47acc30e295a8d992d6b9587d43571
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-03-24 21:26:30 +0800

    updated installer

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/irssi ~/.irssi
-ln -s ~/dotfiles/zshenv ~/.zshenv
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -fs ~/dotfiles/vimrc ~/.vimrc
+ln -fs ~/dotfiles/vim ~/.vim
+ln -fs ~/dotfiles/irssi ~/.irssi
+ln -fs ~/dotfiles/zshenv ~/.zshenv
+ln -fs ~/dotfiles/tmux.conf ~/.tmux.conf
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle && \
-vim +BundleInstall +qall &&
+vim +BundleInstall +qall && \
+sudo pacman -Syu --noconfirm cmake && \
 $HOME/dotfiles/vim/bundle/YouCompleteMe/install.sh --clang-completer
 echo "Vim plugins configured!!" || echo "Error setting up vim directory"
