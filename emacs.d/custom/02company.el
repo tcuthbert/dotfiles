cefef730c8d21c054771bbdadff2f716d63e0937
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-04-02 16:36:26 +0800

    added tab complete

--- emacs.d/custom/02company.el
+++ emacs.d/custom/02company.el
@@ -1,6 +1,14 @@
 ;; Enable global completion
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; Bind TAB to complete or indent
+(defun indent-or-complete ()
+  (interactive)
+  (if (looking-at "\\_>")
+      (company-complete-common)
+    (indent-according-to-mode)))
+(global-set-key (kbd "TAB") 'indent-or-complete)
+
 ;; Python completion
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook 
