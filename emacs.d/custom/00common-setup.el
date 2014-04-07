7c36e0aa823741f136575154f86b838e595ab755
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-04-07 08:57:12 +0000

    updated cask

--- emacs.d/custom/00common-setup.el
+++ emacs.d/custom/00common-setup.el
@@ -48,27 +48,3 @@
 (sp-with-modes '(rhtml-mode)
   (sp-local-pair "<" ">")
   (sp-local-pair "<%" "%>"))
-
-;; Ace jump mode
-;;
-;; ace jump mode major function
-;; 
-(add-to-list 'load-path "/full/path/where/ace-jump-mode.el/in/")
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-;; you can select the key you prefer to
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-;; 
-;; enable a more powerful jump back function from ace jump mode
-;;
-(autoload
-  'ace-jump-mode-pop-mark
-  "ace-jump-mode"
-  "Ace jump back:-)"
-  t)
-(eval-after-load "ace-jump-mode"
-  '(ace-jump-mode-enable-mark-sync))
-(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
