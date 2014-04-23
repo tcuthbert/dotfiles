19d3ee87f422decf5881f334bee0b1afc5fa64f5
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-04-23 14:21:56 +0000

    smartparens now works in tty emacs for erb templates

--- emacs.d/custom/00common-setup.el
+++ emacs.d/custom/00common-setup.el
@@ -55,6 +55,3 @@
 (require 'smartparens-ruby)
 (smartparens-global-mode)
 (show-smartparens-global-mode t)
-(sp-with-modes '(rhtml-mode)
-  (sp-local-pair "<" ">")
-  (sp-local-pair "<%" "%>"))
--- emacs.d/custom/05web.el
+++ emacs.d/custom/05web.el
@@ -8,7 +8,26 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 
 ;; Use smartparens
-(add-hook 'web-mode-hook #'(lambda () (smartparens-mode -1)))
+;(add-hook 'web-mode-hook #'(lambda () (smartparens-mode -1)))
+(defun my-web-mode-hook ()
+  (setq web-mode-enable-auto-pairing nil))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+(defun sp-web-mode-is-code-context (id action context)
+  (when (and (eq action 'insert)
+	     (not (or (get-text-property (point) 'part-side)
+		      (get-text-property (point) 'block-side))))
+
+    t))
+(sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))
+(defun my-erb-tags ()
+  (when (and (stringp buffer-file-name)
+	     (string-match "\\.erb\\'" buffer-file-name))
+    (sp-local-pair 'web-mode "%" "%")))
+
+(add-hook 'find-file-hook 'my-erb-tags)
+
 
 ;; Scss-mode
 (autoload 'scss-mode "scss-mode")
