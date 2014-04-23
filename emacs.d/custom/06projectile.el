3408141317ec6a12bc47e94ccf6bcba499c61e21
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-04-23 08:03:00 +0000

    installed ohmyzsh

--- emacs.d/custom/06projectile.el
+++ emacs.d/custom/06projectile.el
@@ -6,3 +6,6 @@
 (global-set-key (kbd "s-p") 'projectile-find-file)
 ;; Press Command-b for fuzzy switch buffer
 (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+
+;; Project-rails mode
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
