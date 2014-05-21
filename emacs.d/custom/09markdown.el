1fa0b790195cd3c1bb80caf8413bad9f6f60f157
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-05-21 09:36:02 +0000

    updated gitignore

--- emacs.d/custom/09markdown.el
+++ emacs.d/custom/09markdown.el
@@ -1,3 +1,4 @@
 (add-hook 'markdown-mode-hook (lambda () (visual-line-mode t)))
 (setq markdown-command "pandoc --smart -f markdown -t html")
 (setq markdown-css-path (expand-file-name "markdown.css" "~/dotfiles/emacs.d"))
+(add-to-list 'auto-mode-alist '("IMPLEMENTATION\\.md\\'" . gfm-mode))
