5855d9ed17033835c21425e89eb3901b8e840c5e
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-07-02 14:26:52 +0000

    prints standalone html

--- emacs.d/custom/09markdown.el
+++ emacs.d/custom/09markdown.el
@@ -1,4 +1,9 @@
+;;(setq markdown-css-path (expand-file-name "markdown.css" "~/dotfiles/emacs.d"))
+;;(setq markdown-command "pandoc --smart -f markdown -t html")
+
+(defvar css-path (expand-file-name "markdown.css" "~/dotfiles/emacs.d"))
+(defvar mkdcmd
+  (concat "pandoc --smart -f markdown -t html -H " css-path))
 (add-hook 'markdown-mode-hook (lambda () (visual-line-mode t)))
-(setq markdown-command "pandoc --smart -f markdown -t html")
-(setq markdown-css-path (expand-file-name "markdown.css" "~/dotfiles/emacs.d"))
-(add-to-list 'auto-mode-alist '("IMPLEMENTATION\\.md\\'" . gfm-mode))
+(setq markdown-command mkdcmd)
+(add-to-list 'auto-mode-alist '("C[0-9]+\\.md\\'" . gfm-mode))
