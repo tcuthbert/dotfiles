4bbe45284c8b4605f0882e6c7246f0d552db7355
Thomas Cuthbert <tcuthbert90@gmail.com>
2014-08-27 20:30:29 +0800

    updated emacs config

--- emacs.d/custom/11javascript.el
+++ emacs.d/custom/11javascript.el
@@ -1,6 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
-(add-to-list 'load-path "~/.emacs.d/tern/emacs")
+(add-to-list 'load-path "~/.emacs.d/vendor/tern/emacs")
 (autoload 'tern-mode "tern.el" nil t)
 
 ;; Smartparens braces
